f93863929656b3292e1e5a84ac425c0253314d44
songhyeonjun <thdwoqja11@naver.com>
2022-05-03 18:36:37 +0900

    test

---
+++ rproject1/R기본문법.R
@@ -0,0 +1,16 @@
+print('안녕하세여')
+
+a <- 100 # 할당연산자
+b <- 200
+
+print(a + b)
+
+c <- 500 # alt + -
+
+install.packages("glue")
+
+d <- 1:10 # 연속적인 값
+print(d)
+
+e <- c(1, 3:5, 8:10)
+print(e)
---
+++ rproject1/r1.R
@@ -0,0 +1,32 @@
+# 변수에 2개의 넣고,
+## 사칙연산과
+# 더한 값의 절대값(abs())과 
+# 더한 결과값의 루트값(sqrt())을 
+#구해서 실행
+# 나머지 (%%)를 구현해보기 
+
+# packages 탭 사용
+# packages 설치 ggplot2
+
+# 명령어 사용
+# cowsay
+
+a <- 100
+b <- 200
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+print(abs(a + b))
+print(abs(a - b))
+print(abs(a * b))
+print(abs(a / b))
+
+print(sqrt(a + b))
+print(sqrt(a - b))
+print(sqrt(a * b))
+print(sqrt(a / b))
+
+print(a%%b)
